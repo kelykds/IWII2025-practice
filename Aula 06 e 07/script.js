96baa3200950612cb80a8987bb7ba0e0615fb425
Kely <citizenkds@gmail.com>
2026-03-26 18:09:19 +0000

    Adicionando as imagens e testando

--- Aula 06 e 07/script.js
+++ Aula 06 e 07/script.js
@@ -8,11 +8,11 @@ const botao3 = document.getElementById("imagem3");
 
 // Associa o evento de clique
 botao1.addEventListener("click", function() {
-    imagem.src = "img1.jpg";
+    imagem.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8DbLye0-ZSWMUS6NiKkWHqEBsHKJwZ--iOQ&s";
 });
 botao2.addEventListener("click", function() {
-    imagem.src = "img2.jpg";
+    imagem.src = "https://ovicio.com.br/wp-content/uploads/2024/01/20240108-maki2.png";
 });
 botao3.addEventListener("click", function() {
-    imagem.src = "img3.jpg";
+    imagem.src = "https://i.pinimg.com/736x/04/56/36/0456369a471c30ddd24e5b96f70d82a7.jpg";
 });
